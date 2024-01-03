3ea6dfed3ba29695d3ec1ad81048908933b2cd9e
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-03 16:34:32 +0200

    Add colors to the ap

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,26 @@ import 'package:flutter/material.dart';
 // void helps to define a function in flutter
 void main() {
   // this is how a function is called
-  runApp(const MaterialApp(home: Scaffold(body: Text('hello world!'))));
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Color.fromARGB(230, 72, 20, 160),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            )
+          ),
+          child: const Center(
+            child: Text('hello world!'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
