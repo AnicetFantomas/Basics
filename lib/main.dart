6172ba4da619a798c4775d96ac36152375c6e394
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-03 12:27:35 +0200

    Set up mobile emulator for the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 // void helps to define a function in flutter
 void main() {
   // this is how a function is called
-  runApp();
+  runApp(const MaterialApp(home: Scaffold(body: Text('hello world!'))));
 }
