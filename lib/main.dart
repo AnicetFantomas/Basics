fb8a0e02e386a92cb035ba03a31a1c73673f43db
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-03 17:43:24 +0200

    Separate the class

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:Flutter-First-App/Flutter-First';
 
 // void helps to define a function in flutter
 void main() {
   // this is how a function is called
   runApp(
-     MaterialApp(
+     const MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(230, 72, 20, 160),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-            )
-          ),
-          child: const Center(
-            child: Text('hello world!'),
-          ),
-        ),
+        body: GradientContainer()
       ),
     ),
   );
 }
+
