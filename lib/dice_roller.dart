c77ab9c4d4d387895f230fc7c302b436b6c2401d
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-04 21:11:26 +0200

    Display first image and buttons

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -6,33 +6,40 @@ class DiceRoller extends StatefulWidget {
   @override
   State<DiceRoller>createState() {
     return _DiceRollerState();
-  };
+  }
 }
 
 class _DiceRollerState extends State<DiceRoller> {
   var activeDiceImage = 'assets/images/dice-2.png';
 
   void rollDice() {
+    setState(() {
     activeDiceImage = 'assets/images/dice-4.png';
+      
+    });
     // print('Changing image...');
   }
 
   @override
   Widget build(context) {
-    return  Column(mainAxisSize: MainAxisSize.min, children: [
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
         Image.asset(
           activeDiceImage,
           width: 200,
         ),
         const SizedBox(height: 20),
         TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              // padding: const EdgeInsets.only(top: 20),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 28,),
-            ),
-            child: const Text('Roll Dice'))
-      ])),
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(top: 20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
   }
 }
