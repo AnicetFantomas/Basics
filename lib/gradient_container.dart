bf695f163e550f9374c107ece6161125a9b6a89d
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-04 14:52:56 +0200

    Display first image and buttons

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,28 +2,38 @@ import 'package:flutter/material.dart';
 // import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
-const  endAlignment = Alignment.bottomRight;
+const endAlignment = Alignment.bottomRight;
 
-class GradientContainer  extends StatelessWidget {
+class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
   final Color color1;
   final Color color2;
 
+  void rollDice() {
+    
+  }
+
   @override
   Widget build(context) {
-    return  Container(
-          decoration:   BoxDecoration(
-            gradient: LinearGradient(
-              colors:[color1, color2],
-              begin: startAlignment,
-              end: endAlignment,
-            )
-          ),
-          child:  Center(
-            child:Image.asset('assets/images/dice-2.png', width: 200,),
-          ),
-        );
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [color1, color2],
+        begin: startAlignment,
+        end: endAlignment,
+      )),
+      child: Center(
+          child: Column(children: [
+        Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
+        TextButton(onPressed: rollDice,
+         child: const Text('Roll Dice')
+         )
+      ])),
+    );
   }
 }
 
